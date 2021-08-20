705d36bf92dd0e5323b6a2a27cfe34c899ec0a6f
Fuller, Jonathan D <jonathan.d.fuller1@gmail.com>
2021-08-20 08:10:33 -0400

    comment out createstreamonhglobal
    x

--- s2e_win/custom-hook/custom-hook.cpp
+++ s2e_win/custom-hook/custom-hook.cpp
@@ -498,7 +498,7 @@ CyFIFuncType functionToHook[] = {
 
     //CyFIFuncType("Kernel32", "GetCommandLineA", GetCommandLineAHook, {NULL}),
 
-    CyFIFuncType("ole32", "CreateStreamOnHGlobal", CreateStreamOnHGlobalHook, {NULL}),
+    //CyFIFuncType("ole32", "CreateStreamOnHGlobal", CreateStreamOnHGlobalHook, {NULL}),
     //CyFIFuncType("Kernel32", "LoadLibraryW", LoadLibraryWHook, {NULL}),
 
     //CyFIFuncType("Kernel32", "CreateFileA", CreateFileAHook, {NULL}),
