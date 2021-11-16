cacedcc5df3e0bfed568108fe658c9607cf8cad5
jon <jonathan.d.fuller1@gmail.com>
2021-11-16 17:14:19 -0500

    fix internetreadfile
    missing numberofbytestoread as symbolic

--- s2e_win/custom-hook/wininet-hook.cpp
+++ s2e_win/custom-hook/wininet-hook.cpp
@@ -257,6 +257,7 @@ BOOL WINAPI InternetReadFileHook(
 
     std::string tag = GetTag("InternetReadFile");
     S2EMakeSymbolic(lpBuffer, *lpdwNumberOfBytesRead, tag.c_str());
+    S2EMakeSymbolic(lpdwNumberOfBytesRead, 4, tag.c_str());
     Message("[W] InternetReadFile  (%p, %p, 0x%x, %p=0x%x) -> tag_out: %s\n",
         hFile, lpBuffer, dwNumberOfBytesToRead, lpdwNumberOfBytesRead, *lpdwNumberOfBytesRead, tag.c_str());
     return TRUE;
